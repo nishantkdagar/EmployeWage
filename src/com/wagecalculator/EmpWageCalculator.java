7151e6bea0a82ef16ff3c6ca5cfbf32ffaedca2a
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-17 09:43:00 +0530

    Calculating Daily Wage for Full Time

--- src/com/wagecalculator/EmpWageCalculator.java
+++ src/com/wagecalculator/EmpWageCalculator.java
@@ -6,13 +6,20 @@ public class EmpWageCalculator {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Calculator");
         int IS_FULL_TIME=1;
+        int WAGE_PER_HOUR=20;
+        int totalHours=0;
+        int totalWage=0;
         Random random=new Random();
         int empStatus=random.nextInt(2);
         if(empStatus==IS_FULL_TIME) {
             System.out.println("Employee is present");
+            totalHours=8;
+
         }
         else {
             System.out.println("Employee is absent");
         }
+        totalWage=totalHours*WAGE_PER_HOUR;
+        System.out.println("The Total daily wage of Employee is: "+ totalWage);
     }
 }
