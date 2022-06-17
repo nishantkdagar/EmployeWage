ef1d28a3bee25b2ef0c58075b9890ce638a8e2c5
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-17 09:29:27 +0530

    Displaying the Welcome Message

---
+++ src/com/wagecalculator/EmpWageCalculator.java
@@ -0,0 +1,7 @@
+package com.wagecalculator;
+
+public class EmpWageCalculator {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Employee Wage Calculator");
+    }
+}
