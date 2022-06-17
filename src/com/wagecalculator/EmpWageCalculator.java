fda8d3b24b73801c820c699314dfb42e2983aeba
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-17 09:36:02 +0530

    Checking if Employee if present or not

--- src/com/wagecalculator/EmpWageCalculator.java
+++ src/com/wagecalculator/EmpWageCalculator.java
@@ -1,7 +1,18 @@
 package com.wagecalculator;
 
+import java.util.Random;
+
 public class EmpWageCalculator {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Calculator");
+        int IS_FULL_TIME=1;
+        Random random=new Random();
+        int empStatus=random.nextInt(2);
+        if(empStatus==IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
