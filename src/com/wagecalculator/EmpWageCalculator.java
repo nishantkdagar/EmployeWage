3e562aeba7055e652b23bff888e8de2b30dfc6ef
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-24 07:59:50 +0530

    Using Switch Case

--- src/com/wagecalculator/EmpWageCalculator.java
+++ src/com/wagecalculator/EmpWageCalculator.java
@@ -3,24 +3,46 @@ package com.wagecalculator;
 import java.util.Random;
 
 public class EmpWageCalculator {
-    public static void main(String args[]) {
-        System.out.println("Welcome to Employee Wage Calculator");
-        int IS_FULL_TIME=1;
-        int WAGE_PER_HOUR=20;
-        int totalHours=0;
-        int totalWage=0;
-        Random random=new Random();
-        int empStatus=random.nextInt(2);
-        if(empStatus==IS_FULL_TIME) {
-            System.out.println("Employee is present");
-            totalHours=8;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int employeePay = 20;
+    public static final int Required_WORKING_DAYS = 20;
+    public static final int TOTAL_WORKING_HOURS = 100;
+    public static void computeEmpWage(){
+        int totalPay;
+        int totalHour;
+        int totalEmployeeWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmployeeHours = 0;
+        while (totalEmployeeHours <= TOTAL_WORKING_HOURS && totalWorkingDays <= Required_WORKING_DAYS) {
+            for (int day = 1; day <= Required_WORKING_DAYS; day++) {
+                Random r = new Random();
+                int ec = r.nextInt(3);
+                switch (ec) {
+                    case IS_FULL_TIME:
+                        System.out.println("Employee is present");
+                        totalHour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        System.out.println("Employee is present");
+                        totalHour = 4;
+                        break;
+                    default:
+                        totalHour = 0;
+                        System.out.println("Employee is absent");
+                }
 
+                totalPay = totalHour * employeePay;
+                totalEmployeeWage = totalEmployeeWage + totalPay;
+                totalEmployeeHours = totalEmployeeHours + totalHour;
+                System.out.println("The Total earning of a employee: " + totalPay);
+            }
+
+            System.out.println("Employee total income: " + totalEmployeeWage);
+            System.out.println("Employee total working hours: " + totalEmployeeHours);
         }
-        else {
-            System.out.println("Employee is absent");
-            totalHours=4;
-        }
-        totalWage=totalHours*WAGE_PER_HOUR;
-        System.out.println("The Total daily wage of Employee is: "+ totalWage);
+    }
+    public static void main(String[] args) {
+        computeEmpWage();
     }
 }
