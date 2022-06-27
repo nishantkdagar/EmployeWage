a3224b35c74d4f87c9c989e318f1cc9549703e4e
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-27 07:42:27 +0530

    Saving wage for each comapny

--- src/com/wagecalculator/EmpWageCalculator.java
+++ src/com/wagecalculator/EmpWageCalculator.java
@@ -3,35 +3,37 @@ package com.wagecalculator;
 import java.util.Random;
 
 public class EmpWageCalculator {
-    public static int empHours = 0;
-    public static void fullTimeEmployee(int day){
-        System.out.printf("Day %d: Employee is Present for full day.", day).println();
-        empHours = 8;
+    private final String comp;
+    private final int EMP_RATE_PER_HOUR;
+    private final int workingDaysPerMonth;
+    private final int maxWorkingHours;
+    private int totalEmpWage;
+    public EmpWageCalculator(String comp, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours){
+        this.comp = comp;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.workingDaysPerMonth = workingDaysPerMonth;
+        this.maxWorkingHours = maxWorkingHours;
     }
-    public static void partTimeEmployee(int day){
-        System.out.printf("Day %d: Employee worked for half day.", day).println();
-        empHours = 4;
-    }
-    public static void absentEmployee(int day){
-        System.out.printf("Day %d: Employee is Absent.", day).println();
-        empHours = 0;
-    }
-    public static void companyWage(String comp, int EMP_RATE_PER_HOUR, int workingDaysPerMonth, int maxWorkingHours ){
-        int empWage = 0, totalWage=0, workingHours = 0;
+    public void companyWage(){
+        System.out.printf("%s Employee Wage details: ", comp).println();
+        //local variables
+        int empWage = 0, totalWage=0, workingHours = 0, empHours = 0;
         Random random = new Random();
-        //Calculating the total wage per month using loops
         for (int day = 1; day<=workingDaysPerMonth && workingHours<maxWorkingHours; day++) {
 
             int empCheck = random.nextInt(3);
             switch (empCheck) {
                 case 1:
-                    fullTimeEmployee(day);
+                    System.out.printf("Day %d: Employee is Present for full day.", day).println();
+                    empHours = 8;
                     break;
                 case 2:
-                    partTimeEmployee(day);
+                    System.out.printf("Day %d: Employee worked for half day.", day).println();
+                    empHours = 4;
                     break;
                 default:
-                    absentEmployee(day);
+                    System.out.printf("Day %d: Employee is Absent.", day).println();
+                    empHours = 0;
                     break;
             }
             empWage = empHours * EMP_RATE_PER_HOUR;
@@ -39,24 +41,22 @@ public class EmpWageCalculator {
             totalWage += empWage;
             workingHours += empHours;
         }
-        totalWage(comp, totalWage);
-        maxWorkingHours(comp, workingHours);
+        totalEmpWage = workingHours*EMP_RATE_PER_HOUR;
+        System.out.printf("The Total wage earned by %s for this month: " + "$ " + totalWage, comp).println();
+        System.out.printf("Total hours worked for %s: " + workingHours, comp).println();
         System.out.println();
     }
-
-    public static void totalWage(String comp, int wage){
-        System.out.printf("The Total wage earned by %s for this month: " + "$ " + wage, comp).println();
+    public String toString(){
+        return "Total wage earned for the Company " + comp + " is: " + totalEmpWage;
     }
-    public static void maxWorkingHours(String comp, int workingHours){
-        System.out.printf("Total hours worked for %s: " + workingHours, comp).println();
-    }
-
     public static void main(String args[]) {
         //Welcome message
-        System.out.println("Welcome to Employee Wage Calculator. \n");
-        System.out.println("Employee Wage Builder for TCS: \n");
-        companyWage("TCS",20,20,100);
-        System.out.println("Employee Wage Builder for Microsoft: \n");
-        companyWage("Toyota",30,25,120);
+        System.out.println("Welcome to Employee Wage Builder. \n");
+        EmpWageCalculator tcs = new EmpWageCalculator("TCS", 20, 20, 100);
+        tcs.companyWage();
+        EmpWageCalculator microsoft = new EmpWageCalculator("Microsoft", 30, 25, 120);
+        microsoft.companyWage();
+        System.out.println(tcs);
+        System.out.println(microsoft);
     }
 }
